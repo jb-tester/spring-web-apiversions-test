80d4da5a6c213703650a5d1735b199dbd44c4a1f
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-11 19:56:22 +0200

    minor updates to defaultVersion tests

--- src/main/java/com/example/springwebapiversionstest/TestQueryParametersControllerWithDefaults.java
+++ src/main/java/com/example/springwebapiversionstest/TestQueryParametersControllerWithDefaults.java
@@ -17,16 +17,26 @@ public class TestQueryParametersControllerWithDefaults {
 
     @GetMapping(version = "1.0.0")
     public String defaultVersion() {
-        return "defaultVersion";
+        return "explicit version same as defaultVersion (1.0.0)";
     }
 
     @GetMapping(version = "2.0.0")
     public String otherVersion() {
-        return "otherVersion";
+        return "other explicit Version (2.0.0)";
     }
 
     @GetMapping("/test1")
     public String noExplicitVersion1() {
-        return "no explicit Version1";
+        return "test1 no explicit Version";
+    }
+
+    @GetMapping(path = "/test2", version = "1.0.0")
+    public String test2defaultVersion() {
+        return "test2: explicit version same as defaultVersion (1.0.0)";
+    }
+
+    @GetMapping(path = "/test2", version = "2.0.0")
+    public String test2otherVersion() {
+        return "test2: other explicit Version (2.0.0)";
     }
 }
