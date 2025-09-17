bf47363253b0b8f6649ae98e0756b37974964773
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-17 19:49:11 +0200

    multiple providers in the same configurer

--- src/main/java/com/example/springwebapiversionstest/TestMultipleVersionProvidersController.java
+++ src/main/java/com/example/springwebapiversionstest/TestMultipleVersionProvidersController.java
@@ -1,6 +1,8 @@
 package com.example.springwebapiversionstest;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Profile("multiple")
 @RequestMapping("/multiple")
 class TestMultipleVersionProvidersController {
+
+    @GetMapping(version = "1.0.0")
+    public ResponseEntity<String> testV100() {
+        return ResponseEntity.ok("v1.0.0");
+    }
+    @GetMapping(version = "1.2+")
+    public ResponseEntity<String> testV12Plus() {
+        return ResponseEntity.ok("v1.2+");
+    }
+    // duplicates the previous:
+    //@GetMapping(version = "1.2.0")
+    public ResponseEntity<String> testV120() {
+        return ResponseEntity.ok("v1.2.0");
+    }
+
+    @GetMapping(version = "1.2.5")
+    public ResponseEntity<String> testV125() {
+        return ResponseEntity.ok("v1.2.5");
+    }
 
 }
